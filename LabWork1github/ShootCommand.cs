854e48dd9028fa4f37b771b742fde0da039facfa
csw <kulcsar.dana89@gmail.com>
2021-09-27 12:49:03 +0200

    ShootCommand class ready, using it in Visitor is back

--- LabWork1github/ShootCommand.cs
+++ LabWork1github/ShootCommand.cs
@@ -8,5 +8,34 @@ namespace LabWork1github
 {
     public class ShootCommand
     {
+        public int Distance { get; set; } = 1;
+        private string _direction;
+        public int Round { get; set; }
+
+        public string Direction
+        {
+            get { return _direction; }
+            set
+            {
+                if (!value.Equals("F") && !value.Equals("L") && value.Equals("B") && value.Equals("R"))
+                    throw new ArgumentException("wrong direction given");
+                _direction = value;
+            }
+        }
+        public Place targetPlace { get; set; }
+        private int _damage;
+
+        public int Damage
+        {
+            get { return _damage; }
+            set {
+                if(value < 0)
+                    throw new ArgumentException("Negative damage given!");
+                _damage = value;
+            }
+        }
+
+
+        public void Shoot() { }
     }
 }
