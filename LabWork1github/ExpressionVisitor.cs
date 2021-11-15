2686a32782494a48071d29470a2e16adb74ad80a
csw <kulcsar.dana89@gmail.com>
2021-11-15 12:48:18 +0100

    expression in progress, first barrier character.place.x

--- LabWork1github/ExpressionVisitor.cs
+++ LabWork1github/ExpressionVisitor.cs
@@ -27,13 +27,40 @@ namespace LabWork1github
                 ExpressionVisitor helperVisitor = new ExpressionVisitor(ExpressionContext.expression().ElementAt(0));
                 helperVisitor.CheckTypes();
             }
-            if (ExpressionContext.operation() != null) 
-                if (ExpressionContext.operation().ALIVE() != null || ExpressionContext.operation().NEAR() != null)
-                    if (!(ExpressionContext.expression().ElementAt(1).something().NOTHING() != null))
-                        throw new ArgumentException("An operation has more arguments than exprected");
+            if (ExpressionContext.operation() != null)
+                CheckOperation(ExpressionContext);
                     
             VisitExpression(ExpressionContext);
         }
+
+
+        public void CheckOperation(ExpressionContext context)
+        {
+            if (ExpressionContext.operation().ALIVE() != null || ExpressionContext.operation().NEAR() != null)
+                if (!(ExpressionContext.expression().ElementAt(1).something().NOTHING() != null))
+                    throw new ArgumentException("An operation has more arguments than exprected");
+            if(ExpressionContext.operation().NUMCOMPARE() != null || ExpressionContext.operation().NUMCONNECTER() != null)
+            {
+                CheckNumber(ExpressionContext.expression().ElementAt(0));
+                CheckNumber(ExpressionContext.expression().ElementAt(1));
+            }
+            if(ExpressionContext.operation().ATTRIBUTE() != null)
+            {
+                CheckAttribute(ExpressionContext);
+            }
+        }
+
+        private void CheckAttribute(ExpressionContext expressionContext)
+        {
+            if (!(ExpressionContext.expression().ElementAt(0).something().character() != null && ExpressionContext.expression().ElementAt(1).something().possibleAttributes() != null))
+                throw new ArgumentException("Attribute operation used incorrectly");
+            string attribute = ExpressionContext.expression().ElementAt(1).something().possibleAttributes().GetText();
+            switch (attribute)
+            {
+                //TODO: decide if how to deal with me.place.x case grammar changing?
+                }
+        }
+
         public void CheckNumber(ExpressionContext context)
         {
 
