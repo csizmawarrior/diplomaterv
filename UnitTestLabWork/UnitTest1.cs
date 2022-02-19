39735df36c130bb3d3404a4775ad6d0f2a25a1eb
csw <kulcsar.dana89@gmail.com>
2022-02-19 16:47:39 +0100

    testing project created

---
+++ UnitTestLabWork/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using 
+
+namespace UnitTestLabWork
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
