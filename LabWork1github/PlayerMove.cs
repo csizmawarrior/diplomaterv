d81d533eca920e84eda8301186df7948b7d71409
csizmawarrior <kulcsar.dana89@gmail.com>
2022-09-05 20:15:37 +0200

    add lost item in conflict

---
+++ LabWork1github/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWork1github
+{
+    public enum CommandType
+    {
+        move,
+        shoot,
+        health,
+        help
+    }
+
+    public class PlayerMove
+    {
+        public CommandType CommandType { get; set; }
+        public string Direction { get; set; }
+    }
+}
