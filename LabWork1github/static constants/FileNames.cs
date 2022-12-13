505dd5d4d5ca2d7a6af6f464de893ebc4881d938
csizmawarrior <kulcsar.dana89@gmail.com>
2022-12-13 18:57:31 +0100

    put important files to the debug folder

--- LabWork1github/static constants/FileNames.cs
+++ LabWork1github/static constants/FileNames.cs
@@ -8,12 +8,12 @@ namespace LabWork1github.static_constants
 {
     public static class FileNames
     {
-        public const string ERROR_LOG_FILE_ADDRESS = "C:/Users/Dana/antlrworks/error_log.txt";
-        public const string DEFAULT_MONSTER_FILE_ADDRESS = "C:/Users/Dana/antlrworks/DefaultMonster.txt";
-        public const string DEFAULT_TRAP_FILE_ADDRESS = "C:/Users/Dana/antlrworks/DefaultTrap.txt";
-        public const string DEFAULT_BOARD_FILE_ADDRESS = "C:/Users/Dana/antlrworks/BoardCreation.txt";
-        public const string EXAMPLE_MONSTERS_FILE_ADDRESS = "C:/Users/Dana/antlrworks/ExampleMonsters.txt";
-        public const string EXAMPLE_TRAPS_FILE_ADDRESS = "C:/Users/Dana/antlrworks/ExampleTraps.txt";
-        public const string EXAMPLE_BOARD_FILE_ADDRESS = "C:/Users/Dana/antlrworks/ExampleBoard.txt";
+        public const string ERROR_LOG_FILE_ADDRESS = "files/error_log.txt";
+        public const string DEFAULT_MONSTER_FILE_ADDRESS = "files/DefaultMonster.txt";
+        public const string DEFAULT_TRAP_FILE_ADDRESS = "files/DefaultTrap.txt";
+        public const string DEFAULT_BOARD_FILE_ADDRESS = "files/BoardCreation.txt";
+        public const string EXAMPLE_MONSTERS_FILE_ADDRESS = "files/ExampleMonsters.txt";
+        public const string EXAMPLE_TRAPS_FILE_ADDRESS = "files/ExampleTraps.txt";
+        public const string EXAMPLE_BOARD_FILE_ADDRESS = "files/ExampleBoard.txt";
     }
 }
